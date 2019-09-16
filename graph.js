eb017677033af90064530ae811f6980940902440
Teustoscano <ssstoscano@gmail.com>
2019-09-16 10:46:52 -0300

    .

--- graph.js
+++ graph.js
@@ -20,4 +20,46 @@ const lowCostNode = (cost, processed) => {
 
 const dijkstra = (graph) => {
     const costs = Object.assign({finish: Infinity}, graph.start);
-}
+
+    const parents = {finish: null};
+    for (let child in graph.start){
+        parents[child] = 'start';
+    }
+
+    const processed = [];
+
+    while(node){
+        let cost = costs[node];
+        let children = graph[node];
+        for (let n in children){
+            let newCost = cost + children[n];
+            if(!costs[n]){
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+            if(costs[n] > newCost){
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+        }
+        processed.push(node);
+        node = lowCostNode(costs, processed);
+    }
+
+    let optimalPath = ['finish'];
+    let parent = parents.finish;
+    while(parent) {
+        optimalPath.push(parent);
+        parent = parents[parent];
+    }
+    optimalPath.reverse();
+
+    const results = {
+        distance: costs.finish,
+        path: optimalPath
+    };
+
+    return results;
+};
+
+console.log(dijkstra(problem))
