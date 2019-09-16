2af71f8d06bdbd69ad504deba18c80329a5b5550
Teustoscano <ssstoscano@gmail.com>
2019-09-16 11:03:55 -0300

    .

--- graph.js
+++ graph.js
@@ -1,16 +1,23 @@
 const problem = {
-    start:  {A: 5, B: 2},
-    A: {C: 4, D: 2},
+    start: {A: 5, B: 2},
+    A: {start: 1, C: 4, D: 2},
     B: {A: 8, D: 7},
     C: {D: 6, finish: 3},
     D: {finish: 1},
     finish: {}
 };
 
-const lowCostNode = (cost, processed) => {
-    return Object.keys(cost).reduce((lowest, node) => {
-        if(lowest === null || cost[node] < cost[lowest]){
-            if(!processed.includes(node)){
+function log(message) {
+    const logging = false;
+    if (logging) {
+        console.log(message);
+    }
+}
+
+const lowestCostNode = (costs, processed) => {
+    return Object.keys(costs).reduce((lowest, node) => {
+        if (lowest === null || costs[node] < costs[lowest]) {
+            if (!processed.includes(node)) {
                 lowest = node;
             }
         }
@@ -18,49 +25,66 @@ const lowCostNode = (cost, processed) => {
     }, null);
 };
 
-const dijkstra = (graph) => {
-    const costs = Object.assign({finish: Infinity}, graph.start);
+// function that returns the minimum cost and path to reach Finish
+const dijkstra = (graph, startNodeName, endNodeName) => {
 
-    const parents = {finish: null};
-    for (let child in graph.start){
-        parents[child] = 'start';
+    // track the lowest cost to reach each node
+    let costs = {};
+    costs[endNodeName] = "Infinity";
+    costs = Object.assign(costs, graph[startNodeName]);
+
+    // track paths
+    const parents = {endNodeName: null};
+    for (let child in graph[startNodeName]) {
+        parents[child] = startNodeName;
     }
 
+    // track nodes that have already been processed
     const processed = [];
-    let node = lowCostNode(costs, processed);
 
-    while(node){
+    let node = lowestCostNode(costs, processed);
+
+    while (node) {
         let cost = costs[node];
         let children = graph[node];
-        for (let n in children){
-            let newCost = cost + children[n];
-            if(!costs[n]){
-                costs[n] = newCost;
-                parents[n] = node;
-            }
-            if(costs[n] > newCost){
-                costs[n] = newCost;
-                parents[n] = node;
+        for (let n in children) {
+            if (String(n) === String(startNodeName)) {
+                log("WE DON'T GO BACK TO START");
+            } else {
+                log("StartNodeName: " + startNodeName);
+                log("Evaluating cost to node " + n + " (looking from node " + node + ")");
+                log("Last Cost: " + costs[n]);
+                let newCost = cost + children[n];
+                log("New Cost: " + newCost);
+                if (!costs[n] || costs[n] > newCost) {
+                    costs[n] = newCost;
+                    parents[n] = node;
+                    log("Updated cost und parents");
+                } else {
+                    log("A shorter path already exists");
+                }
             }
         }
         processed.push(node);
-        node = lowCostNode(costs, processed);
+        node = lowestCostNode(costs, processed);
     }
 
-    let optimalPath = ['finish'];
-    let parent = parents.finish;
-    while(parent) {
+    let optimalPath = [endNodeName];
+    let parent = parents[endNodeName];
+    while (parent) {
         optimalPath.push(parent);
         parent = parents[parent];
     }
     optimalPath.reverse();
 
     const results = {
-        distance: costs.finish,
+        distance: costs[endNodeName],
         path: optimalPath
     };
 
     return results;
 };
 
-console.log(dijkstra(problem))
+console.log(dijkstra(problem, "start", "finish"));
+console.log(dijkstra(problem, "A", "B"));
+console.log(dijkstra(problem, "A", "start"));
